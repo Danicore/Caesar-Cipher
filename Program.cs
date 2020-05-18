cbb3ebdafa12bacd7f18fd1a4dbcbc3f4c5654e7
dani <groddpapyrus@yahoo.com>
2020-05-18 21:56:45 +0500

    program re run

--- Program.cs
+++ Program.cs
@@ -1,20 +1,30 @@
-﻿/*
-PAF KIET UNIVERSITY PROJECT
-Subject: Programming Fundamentals 103471
-Semester: Spring 2020
-Topic: Caesar Cipher
-Made By:
-1. Danial Farooq Haider 11084
-2. Yousuf Baloch 11495
-3. Muhammad Haroon 11204
-*/
-
-using System;
+﻿using System;
 
 namespace PF_Project
 {
     class Program
     {
+        static string Caesar(string value, int shift)
+        {
+            char[] buffer = value.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                // Add shift to all.
+                buffer[i] = Convert.ToChar(buffer[i] + shift);
+                // Subtract 26 on overflow.
+                if (buffer[i] > 'Z')
+                {
+                    buffer[i] = Convert.ToChar(buffer[i] - 26);
+                }
+                // Add 26 on underflow.
+                else if (buffer[i] < 'A')
+                {
+                    buffer[i] = Convert.ToChar(buffer[i] + 26);
+                }
+            }
+            return new string(buffer);
+        }
+        
         static void Main()
         {
             Console.Write("Please enter 'E' to encrypt or 'D' to decrypt: ");
@@ -45,27 +55,18 @@ namespace PF_Project
                 Console.WriteLine(encryptedInput);
                 Console.WriteLine(decryptedOutput);
             }
+
+            Repeat();
         }
 
-        static string Caesar(string value, int shift)
+        static void Repeat()
         {
-            char[] buffer = value.ToCharArray();
-            for (int i = 0; i < buffer.Length; i++)
+            Console.WriteLine("Do you want to encrypt or decrypt anything else? Enter 'Y' for yes or 'N' for no: ");
+            string more = Console.ReadLine().ToUpper();
+            if (more == "Y")
             {
-                // Add shift to all.
-                buffer[i] = Convert.ToChar(buffer[i] + shift);
-                // Subtract 26 on overflow.
-                if (buffer[i] > 'Z')
-                {
-                    buffer[i] = Convert.ToChar(buffer[i] - 26);
-                }
-                // Add 26 on underflow.
-                else if (buffer[i] < 'A')
-                {
-                    buffer[i] = Convert.ToChar(buffer[i] + 26);
-                }
+                Main();
             }
-            return new string(buffer);
         }
     }
 }
